ab4ad16b4f0ddbe2988f096cc44279f80159b964
Alex Dudley <alexdudley@me.com>
2021-04-01 16:01:01 -0500

    stopping for the day

--- src/components/Kennel.js
+++ src/components/Kennel.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { AnimalCard } from "./animal/AnimalCard";
 import "./Kennel.css";
+import { CustomerCard } from "./customer/Customer";
 
 export const Kennel = () => (
 	<>
@@ -12,11 +13,18 @@ export const Kennel = () => (
 			<div>500 Puppy Way</div>
 		</address>
 
-    <h2>Animals</h2>
-    <article className="animals">
-      <AnimalCard />
-      <AnimalCard />
-      <AnimalCard />
-    </article>
+		<h2>Animals</h2>
+		<article className="animals">
+			<AnimalCard />
+			<AnimalCard />
+			<AnimalCard />
+		</article>
+
+		<h2>Customers</h2>
+		<article className="customers">
+			<CustomerCard />
+			<CustomerCard />
+			<CustomerCard />
+		</article>
 	</>
 );
---
+++ src/components/employee/Employee.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import './Employee.css'
+
+export const EmployeeCard = () => (
+  <section className="employee">
+    <h3 className="employee__name">Morty Smith</h3>
+    <div className="employee__job">Job Title: Pooper Scooper Upper</div>
+  </section>
+)
